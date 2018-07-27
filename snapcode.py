06fe9b736ec1b7c4f2eedfbe880ebce2cf355c78
sdushantha <siddharth.dushantha@gmail.com>
2018-07-27 08:45:31 +0200

    More user friendly

--- snapcode.py
+++ snapcode.py
@@ -2,56 +2,53 @@ import argparse
 import sys
 import urllib.request
 
-__version__ = "v1.0"
-url = "https://app.snapchat.com/web/deeplink/snapcode?username={}&type={}&bitmoji={}"
+url = "https://app.snapchat.com/web/deeplink/snapcode?username={}&type={}"
 
 
-def get_code(username, bitmoji_option):
+def get_code(username, bitmoji):
 
-	if bitmoji_option == False:
+	if bitmoji == False:
 
-		bitmoji_option = "disable"
+		# If filetype is PNG, you will get the 
+		# SnapCode without the bitmoji
+
 		filetype = "PNG"
 
 	else:
-		bitmoji_option = "enable"
+
+		# If filetype is SVG, you will get the 
+		# SnapCode with the bitmoji
+
 		filetype = "SVG"
 
-	request_url = url.format(username, filetype, bitmoji_option)
+	# Putting all the information into the url
+	request_url = url.format(username, filetype)
 
-	path = str(username)+"."+filetype.lower()
+	# Making the filename
+	path = str(username) + "." + filetype.lower()
 
+	# Downloading the image file
 	urllib.request.urlretrieve(request_url, path)
-	print("Downloaded ---> "+path)
+
+	print("Downloaded ---> " + path)
 
 
 def main():
 
 	parser = argparse.ArgumentParser(description = "A simple command-line tool to download SnapChat codes.")
 
-	parser.add_argument("-v", "--version",
-		action='store_true',
-		help = "Get current version")
+	parser.add_argument('username', action = "store")
 
-	parser.add_argument("-u", "--username",
-		type=str,
-		required=True,
-		help = "SnapChat username")
-
-	parser.add_argument("-b", "--bitmoji_option", type = bool,
-						default = False,
-						choices=[True, False],
-						help = "If you want a Bitmoji in the center of the snapcode")
+	parser.add_argument("-b", "--bitmoji",
+		                action= "store_true",
+						help = "SnapCode with Bitmoji")
 
 	args = parser.parse_args()
 
-	if len(sys.argv) == 1: # if argument is given then show help
+	if len(sys.argv) == 1: # if no argument is given then show help
 		parser.print_help()
 
-	elif args.version:
-		print(__version__)
-
-	get_code(args.username, args.bitmoji_option)
+	get_code(args.username, args.bitmoji)
 
 if __name__=="__main__":
 	main()
